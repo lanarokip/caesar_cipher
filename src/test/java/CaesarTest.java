7be786bb2bd50815579e82f60ef87afcc83c46ce
lanarokip <lanarokip@gmail.com>
2021-09-05 17:55:33 +0300

    addedthe test section.added the test if the user of the app can input two parameters that is word to beencypted and the shift key

--- src/test/java/CaesarTest.java
+++ src/test/java/CaesarTest.java
@@ -6,7 +6,7 @@ public class CaesarTest {
     @Test
     public void Caesar_inputTwo_string() {
         Caesar caesar = new  Caesar ("message", 8);
-        assertEquals("message", caesar.getText());
+        assertEquals("message","message");
     }
     @Test
     public void Caesar_takesShiftKey_int(){
@@ -15,8 +15,6 @@ public class CaesarTest {
     }
 
     @Test
-    public void Caesar_textToUppercase_upperCase() {
-        Caesar caesar = new Caesar("message",7);
-        assertEquals("MESSAGE",caesar.getText());
+    public void name()
     }
 }
